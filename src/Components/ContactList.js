133483dce9a3f907fbca0c2e1d39dd9a5a5e3659
NarendraVarma Uppalapati <narendravarma.u@ad.infosys.com>
2020-10-17 18:42:48 +0530

    contact list

---
+++ src/Components/ContactList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Contact} from "./Contact"
+
+export const ContactList = () => {
+    return (
+        <div>
+            <h2 style={{ textAlign: "center" }}>Create Contact</h2>
+            <div>
+                <Contact />
+            </div>
+        </div>
+
+    )
+}
